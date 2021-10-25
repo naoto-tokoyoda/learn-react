b648459e11ca885ac4382db753d0271e4b15c000
Naoto Tokoyoda <tkyd89@gmail.com>
2021-10-25 17:07:00 +0900

    styled components are added

--- style-in-React/src/components/UI/Button/Button.js
+++ style-in-React/src/components/UI/Button/Button.js
@@ -1,13 +1,45 @@
-import React from 'react';
 
-import './Button.css';
+// import './Button.css';
+import styled from 'styled-components';
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+/*
+
+  This is not as global css.
+  since we have unique class names for every styled component, the style set up here
+  will never be able to affect the number of component in the app.
+  Because these unique class names are really well unique per style component.
+
+  you can check button for css using developer tool on website.
+  class name is unique
+
+*/
+const Button = styled.button`
+  
+    font: inherit;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid #1d3557;
+    color: white;
+    background: #1d3557;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #495057;
+    border-color: #495057;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
+  }
+`;
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
