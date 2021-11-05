1d4c264f57dbc9e74d28e5d55e8638ed4a63acd2
Naoto Tokoyoda <tkyd89@gmail.com>
2021-11-05 16:18:13 +0900

    updated

--- style-in-React/src/components/UI/Button/Button.js
+++ style-in-React/src/components/UI/Button/Button.js
@@ -1,6 +1,9 @@
 
-// import './Button.css';
-import styled from 'styled-components';
+// I installed from styled-component, so I can use this.
+// import styled from 'styled-components';
+
+//using CSS as modules 
+import styles from './Button.module.css';
 
 /*
 
@@ -13,33 +16,36 @@ import styled from 'styled-components';
   class name is unique
 
 */
-const Button = styled.button`
+// const Button = styled.button`
+//     width:100%;
+//     font: inherit;
+//     padding: 0.5rem 1.5rem;
+//     border: 1px solid #1d3557;
+//     color: white;
+//     background: #1d3557;
+//     box-shadow: 0 0 4px rgba(0, 0, 0, 0.3);
+//     cursor: pointer;
   
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #1d3557;
-    color: white;
-    background: #1d3557;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.3);
-    cursor: pointer;
+//   @media (min-width: 768px) {
+//     width:auto;
+//   }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:focus {
-    outline: none;
-  }
-
-  &:hover,
-  &:active {
-    background: #495057;
-    border-color: #495057;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
-  }
-`;
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+//   &:hover,
+//   &:active {
+//     background: #495057;
+//     border-color: #495057;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.3);
+//   }
+// `;
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
